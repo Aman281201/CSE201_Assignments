a58a2477c64bc3dfff0270011ac31a3c9efca3c0
Aman Kumar <cloud9xpress@Amans-MacBook-Air.local>
2021-11-06 23:35:42 +0530

    started assignment 3

---
+++ MatrixOperations_Assign3/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+    public void printMenu()
+    {
+        System.out.println("""
+                1.  Take Matrix as input
+                2.  Create matrices of requested types
+                3.  Change matrix element
+                4.  Display all the labels of matrix
+                5.  Perform addition subtraction or multiplication
+                6.  Perform element wise operations
+                7.  Transpose matrix
+                8.  Invert matrix
+                9.  Compute mean
+                10. Compute determinant
+                11. Use singleton as scalars
+                12. Compute the sum of matrix and its transpose
+                13. Compute eigen vectors and values (only 2x2 matrix)
+                14. Solve sets of linear equations
+                15. Retrieve all matrix having requested label
+                """);
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
