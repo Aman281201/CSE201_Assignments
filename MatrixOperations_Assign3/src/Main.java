ea094e2332f43577a305132365caed56755050aa
Aman Kumar <cloud9xpress@Amans-MacBook-Air.local>
2021-11-10 17:57:57 +0530

    completed task1

--- MatrixOperations_Assign3/src/Main.java
+++ MatrixOperations_Assign3/src/Main.java
@@ -1,7 +1,4 @@
-import Classes.DiagonalScalar;
-import Classes.Matrix;
-import Classes.NullOnes;
-import Classes.Singleton;
+import Classes.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -79,6 +76,120 @@ public class Main {
          }
 
     }
+
+    public static void printTranspose(Matrix matrix)
+    {
+        if(matrix.getNull())
+        {
+            for(int i = 0; i < matrix.getCollumn(); i++)
+            {
+                for(int j = 0; j < matrix.getRow(); j++)
+                    System.out.println(0 + "\t");
+
+                System.out.println("\n");
+            }
+            System.out.println("\n");
+            return;
+        }
+        if(matrix.getOnes())
+        {
+            for(int i = 0; i < matrix.getCollumn(); i++)
+            {
+                for(int j = 0; j < matrix.getRow(); j++)
+                    System.out.println(1 + "\t");
+
+                System.out.println("\n");
+            }
+            System.out.println("\n");
+            return;
+        }
+
+        if(matrix.getSingleton())
+        {
+            System.out.println(((Singleton)matrix).getMat() + "\n");
+            return;
+        }
+
+        if(matrix.getId())
+        {
+            for(int i = 0; i < matrix.getRow(); i++)
+            {
+                for(int j = 0; j < matrix.getCollumn(); j++)
+                    if(i == j)
+                        System.out.println(1 + "\t");
+                    else System.out.println(0 + "\t");
+
+                System.out.println("\n");
+            }
+            System.out.println("\n");
+            return;
+        }
+        if(matrix.getDiag())
+        {
+            ArrayList<Integer> d = ((DiagonalScalar)matrix).getDiagonal();
+            for(int i = 0; i < matrix.getRow(); i++)
+            {
+                for(int j = 0; j < matrix.getCollumn(); j++)
+                    if(i == j)
+                        System.out.println( d.get(i) + "\t");
+                    else System.out.println(0 + "\t");
+
+                System.out.println("\n");
+            }
+            System.out.println("\n");
+            return;
+        }
+
+    }
+
+    public static int getDet(ArrayList<ArrayList<Integer>> mat, int n)
+    {
+
+            int num1, num2, det = 1, index, total = 1,w;
+
+            int[] temp = new int[n + 1];
+
+
+            for (int i = 0; i < n; i++) {
+                index = i;
+
+
+                while (mat.get(index).get(i) == 0 && index < n) {
+                    index++;
+                }
+                if (index == n)
+                    continue;
+
+                if (index != i) {
+                    for (int j = 0; j < n; j++) {
+                        w = mat.get(index).get(j);
+                        mat.get(index).set(j,(mat.get(i).get(j)));
+                        mat.get(i).set(j, w);
+                    }
+                    det = (int)(det * Math.pow(-1, index - i));
+                }
+
+                for (int j = 0; j < n; j++) {
+                    temp[j] = mat.get(i).get(j);
+                }
+
+                for (int j = i + 1; j < n; j++) {
+                    num1 = temp[i];
+                    num2 = mat.get(j).get(i);
+
+                    for (int k = 0; k < n; k++) {
+                        mat.get(j).set(k , num1 * mat.get(j).get(k) - num2 * temp[k]);
+                    }
+                    total = total * num1;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                det = det * mat.get(i).get(i);
+            }
+            return (det / total);
+    }
+
     public static void printMenu()
     {
         System.out.println("""
@@ -103,7 +214,7 @@ public class Main {
 
     public static void getInput()
     {
-        int r, c, x, count0 = 0, count1 = 0, countId = 0;
+        int r, c, x, count0 = 0, count1 = 0, countId = 0, countScalar = 0, temp = 0;
         ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
         System.out.println("enter the total number of rows in the matrix");
         r = sc.nextInt();
@@ -126,6 +237,15 @@ public class Main {
                     // identity scalar triangular
                     if (i != j && x != 0)
                         countId++;
+                    if(i == j)
+                    {
+                        if(i == 0) {
+                            temp = x;
+                            countScalar++;
+                        }
+                        else if (x == temp)
+                            countScalar++;
+                    }
                 }
 
             }
@@ -135,6 +255,62 @@ public class Main {
         if(count0 == r*c)
         {
             NullOnes m = new NullOnes(r,c,false);
+            matrices.add((Matrix) m);
+        }
+        else if(count1 == r*c)
+        {
+            NullOnes m = new NullOnes(r,c,true);
+            matrices.add((Matrix) m);
+        }
+        else if(r == c) {
+            if (countId == 0 && count1 == 3) {
+                Identity m = new Identity(r, c);
+                matrices.add((Matrix) m);
+            }
+            else if (r == 1) {
+                Singleton m = new Singleton(mat.get(0).get(0));
+                matrices.add((Matrix) m);
+            }
+            else if (countId == r * c - r) {
+
+                ArrayList<Integer> d = new ArrayList<>();
+                for(int z = 0; z < r; z++)
+                    d.add(mat.get(z).get(z));
+
+                if (countScalar == r) {
+                    DiagonalScalar m = new DiagonalScalar(r, c,d , true);
+                    matrices.add((Matrix) m);
+                }
+                else {
+                    DiagonalScalar m = new DiagonalScalar(r,c,d,false);
+                    matrices.add((Matrix) m);
+                }
+            }
+            else {
+                int det = 0;
+                det = getDet(mat,r);
+                SquareSymSkewSingTri m = new SquareSymSkewSingTri(r,c,mat,false,false,false,false,det);
+                matrices.add((Matrix) m);
+            }
+        }
+        else{
+            if(r == 1)
+            {
+                RowCol m = new RowCol(r,c,mat.get(0),true);
+                matrices.add((Matrix) m);
+            }
+            else if (c == 1)
+            {
+                ArrayList<Integer> col = new ArrayList<>();
+                for(int k = 0; k < c; k++)
+                    col.add(mat.get(0).get(k));
+                RowCol m = new RowCol(r,c,col,false);
+                matrices.add((Matrix) m);
+            }
+            else{
+                Rectangle m = new Rectangle(r,c,mat);
+                matrices.add((Matrix) m);
+            }
         }
 
     }
@@ -166,6 +342,18 @@ public class Main {
 
     public static void getTranspose()
     {
+        System.out.println("choose the matrix\n");
+        for(int i = 0 ; i < matrices.size(); i++)
+        {   System.out.printf(i + ")   ");
+            printMatrix(matrices.get(i));
+        }
+
+        int choice = sc.nextInt();
+
+        Matrix ch = matrices.get(choice);
+
+        printTranspose(ch);
+
 
     }
 
@@ -232,7 +420,7 @@ public class Main {
             printMenu();
             res = sc.nextInt();
 
-            switch (res)
+            switch(res)
             {
                 case 1: getInput();
                     break;
